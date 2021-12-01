1981748d95e6fe0c83f2ac57f721339d0b704acf
James Angier <james@angier.co.uk>
2021-12-01 20:42:21 +1000

    active line on nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,20 @@
 import styles from './NavBar.module.css';
 import logo from '../../assets/cblLge.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import facebook from '../../assets/facebook.svg';
 import instagram from '../../assets/instagram.svg';
 import mail from '../../assets/mail.svg';
+import { useEffect, useState } from 'react';
 
 function NavBar() {
+  const url = window.location.pathname;
+  const navigate = useNavigate();
+  const [activeLink, setActiveLink] = useState('/');
+
+  useEffect(() => {
+    setActiveLink(url);
+  }, [setActiveLink, url, navigate]);
+
   return (
     <div className={styles.navContainer}>
       <div className={styles.navLogo}>
@@ -14,11 +23,38 @@ function NavBar() {
         </Link>
       </div>
       <nav className={styles.navLinks}>
-        <Link to="/behindthebrand">Behind the Brand</Link>
-        <Link to="/menu">Menu and Pricing</Link>
-        <Link to="/cakedesigns">Cake Designs</Link>
-        <Link to="/loyalty">Loyalty Program</Link>
-        <Link to="/contact">Contact Us</Link>
+        <Link
+          to="/behindthebrand"
+          className={
+            activeLink === '/behindthebrand' ? styles.navLinkActive : ''
+          }
+        >
+          Behind the Brand
+        </Link>
+        <Link
+          to="/menu"
+          className={activeLink === '/menu' ? styles.navLinkActive : ''}
+        >
+          Menu and Pricing
+        </Link>
+        <Link
+          to="/cakedesigns"
+          className={activeLink === '/cakedesigns' ? styles.navLinkActive : ''}
+        >
+          Cake Designs
+        </Link>
+        <Link
+          to="/loyalty"
+          className={activeLink === '/loyalty' ? styles.navLinkActive : ''}
+        >
+          Loyalty Program
+        </Link>
+        <Link
+          to="/contact"
+          className={activeLink === '/contact' ? styles.navLinkActive : ''}
+        >
+          Contact Us
+        </Link>
       </nav>
       <div className={styles.navSocial}>
         <a href="https://www.facebook.com/cakedbylaura25/">
